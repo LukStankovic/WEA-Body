322940b873a37d13c99b9b4b9de65ebb6e436a7b
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-01-24 12:49:34 +0100

    Metoda vzdálenosti bodu od nuly

--- bod.php
+++ bod.php
@@ -7,6 +7,9 @@ class Bod{
         $this->x=$x;
         $this->y=$y;
     }
-
+    
+    public function vzdalenostOdNuly(){
+        return sqrt( pow((0-$this->x),2) + pow((0-$this->y),2) );
+    }
 }
 ?>
